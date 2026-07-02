0dbbc212821a59b211dac6c6a9ec4da47e719ad7
Marco <marcosaudisio@gmail.com>
2026-07-02 00:21:28 -0300

    Desactivar modo mantenimiento: mostrar el sitio real

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Para ocultar el sitio y mostrar "estamos trabajando en esto" a las visitas,
 // poné MANADA_MAINTENANCE en true, guardá, hacé commit y push.
 // Para volver a mostrar el sitio real, ponelo en false.
-const MANADA_MAINTENANCE = true;
+const MANADA_MAINTENANCE = false;
